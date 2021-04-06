175cd51e36a3e853b84963837e23847deefa2e5c
senaitsamuel <senaitsam@yahoo.com>
2021-04-06 13:40:01 +0200

    final commit

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React from "react";
 import "./App.css";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Header from "./Components/Header";
 import SideBar from "./Components/SideBar";
 import styled from "styled-components";
